7ee7fc0ca511c6799c05ea208133e13ccf067ed5
Jared Kyle E. Leonano <118417808+JaredKyleLeonano@users.noreply.github.com>
2026-09-21 19:43:04 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string) {
+
+        if (s.length != t.length){
+            return false
+        }
+        const totalCount = new Array(26).fill(0);
+        for (let i = 0; i < s.length; i++){
+            totalCount[s.charCodeAt(i) - 97]++
+            totalCount[t.charCodeAt(i) - 97]--
+        }
+
+        return totalCount.every((count) => count === 0);
+        
+    }
+}
