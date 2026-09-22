10a6100e2d69ac8ff7e6131aac6d2d5d1993cfc1
Jared Kyle E. Leonano <118417808+JaredKyleLeonano@users.noreply.github.com>
2026-09-22 19:32:22 -0400

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const numSet = new Set<number>()
+
+        for(let i = 0; i < nums.length; i++){
+            const toFind = target - nums[i]
+            if(numSet.has(toFind)){
+                return [nums.indexOf(toFind), i]
+            } else {
+                numSet.add(nums[i])
+            }
+        }
+    }
+}
